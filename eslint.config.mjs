d515f272b36333d2948adc9cfe931aebcb5f2654
js.jung <a01027011628@gmail.com>
2025-09-10 17:25:32 +0900

    modify configuration for apply eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,9 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import { prettier } from "eslint-config-prettier";
-import {
-  configs as TanstackQueryConfigs,
-  rules as TanstackQueryRules,
-} from "@tanstack/eslint-plugin-query";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+import pluginQuery from "@tanstack/eslint-plugin-query";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,20 +14,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...pluginQuery.configs["flat/recommended"],
+  eslintConfigPrettier,
   {
     ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
-    plugins: {
-      "@tanstack/eslint-plugin-query": { rules: TanstackQueryRules, configs: TanstackQueryConfigs },
-    },
-    rules: {
-      "@tanstack/query/exhaustive-deps": "error",
-      "@tanstack/query/no-deprecated-options": "error",
-      "@tanstack/query/prefer-query-object-syntax": "error",
-      "@tanstack/query/stable-query-client": "error",
-    },
-    extends: ["plugin:@tanstack/eslint-plugin-query/recommended"],
   },
-  prettier,
 ];
 
 export default eslintConfig;
